6800077546c134f9bdf2bd493ac84ca614780e35
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-07-14 21:47:35 +0500

    Updated

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,19 @@
-import React from "react";
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 const Intro = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +26,7 @@ const Intro = () => {
           <h2>Hi There, I'm</h2>
           <h1>Maroof Qureshi</h1>
           <h3>
-            Web Developer<span></span>
+            Freelance <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,40 @@ import "./portfolio.scss";
 const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
-      Portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 };

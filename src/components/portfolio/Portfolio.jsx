ae0b905a69a8bd010791a6cfabc408f23da41ab4
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-07-15 21:49:58 +0500

    Updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 import "./portfolio.scss";
 import PortfolioList from "../portfolioList/PortfolioList";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 const Portfolio = () => {
   const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
   const list = [
     {
       id: "featured",
@@ -27,6 +35,28 @@ const Portfolio = () => {
     },
   ];
 
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
@@ -41,30 +71,12 @@ const Portfolio = () => {
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="assets" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets" alt="" />
-          <h3>Banking App</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -4,7 +4,28 @@ import "./works.scss";
 const Works = () => {
   return (
     <div className="works" id="works">
-      Works
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Exercitationem pariatur cupiditate temporibus voluptate saepe
+                  quae aliquid eius? Atque dolore, ex voluptatem sapiente
+                  pariatur ipsa optio ad. Quam amet quasi voluptatem.
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
